a9bdf4dc6a10868d7f37670b5216307fcca8d54e
Michael Poniente <michaelwrites.biz@gmail.com>
2026-06-06 01:35:33 -0700

    fixed home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,18 @@
 import MomentoMap from "@/components/map/MomentoMap"
+import NavBar from "@/components/ui/NavBar"
 
-export default function Home() {
-  return <MomentoMap />
+export default function MapPage() {
+    return (
+        <div style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            overflow: "hidden"
+        }}>
+            <MomentoMap />
+            <NavBar />
+        </div>
+    )
 }
