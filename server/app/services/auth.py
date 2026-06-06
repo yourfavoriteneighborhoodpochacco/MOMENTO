7921d30adcca453c8be7208dcb324e23f8e54354
Michael Poniente <michaelwrites.biz@gmail.com>
2026-06-06 02:34:21 -0700

    added admin as a contributor

--- server/app/services/auth.py
+++ server/app/services/auth.py
@@ -65,7 +65,7 @@ def get_current_user(
 def get_current_contributor(
     current_user: User = Depends(get_current_user)
 ) -> User:
-    if current_user.account_type != "contributor":
+    if current_user.account_type not in ("contributor", "admin"):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Only contributors can perform this action"
