a0011b7dfc1081fe08892aab4e6fc57dc956a401
Michael Poniente <michaelwrites.biz@gmail.com>
2026-06-06 01:03:53 -0700

    update CORS for production

--- server/app/main.py
+++ server/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="momento API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://momento.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://join-momento.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
